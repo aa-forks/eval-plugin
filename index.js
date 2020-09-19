58108287f64bea553a13cac19a047439702305e5
aesthetical <57580886+Sxmurai@users.noreply.github.com>
2020-09-19 13:37:11 -0500

    new code including autocomplete!

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const { Plugin } = require("powercord/entities");
 const { inspect } = require("util");
 
+const {
+  functions: { isPromise, type },
+  Parser,
+} = require("./util");
+
 const Settings = require("./Settings.jsx");
 
 module.exports = class EvalCommand extends Plugin {
   startPlugin() {
+    const autoComplete = (powercord.pluginManager.plugins.get(
+      "pc-evalcommand"
+    ).messages =
+      powercord.pluginManager.plugins.get("pc-evalcommand").messages ?? []);
+
     const replace = this.settings.get("tokenReplacer", "[REDACTED]");
     const format = this.settings.get(
       "evalFormat",
       "⏱️ Took {time}{n}🔍 Typeof {type}{n}{output}"
     );
+
+    const autoCompleteAmount = this.settings.get("autoCompleteAmount", 25);
+    autoComplete.slice(0, autoCompleteAmount);
 
     powercord.api.settings.registerSettings("pc-evalcommand", {
       category: this.entityID,
@@ -20,25 +33,44 @@ module.exports = class EvalCommand extends Plugin {
     powercord.api.commands.registerCommand({
       command: "eval",
       aliases: ["evaluate"],
-      description: "Evaluates JavaScript code",
-      usage: "{c} [ code ]",
+      description: "Evaluates any JavaScript code",
+      usage: "{c} [ code ] [ -depth=<number> ]",
       category: "Util",
       async executor(args) {
-        try {
-          const hrstart = process.hrtime();
-          let toEval = eval(args.join(" "));
-          const hr = process.hrtime(hrstart);
+        if (!args.length)
+          return {
+            send: false,
+            result: `Invalid usage! Valid Usage: \`${this.usage.replace(
+              "{c}",
+              powercord.api.commands.prefix + this.command
+            )}\``,
+          };
 
-          if (EvalCommand.isPromise(toEval)) {
+        const parser = new Parser();
+        const { flags, phrases } = parser.parse(
+          args.join(" ").replace(/```js/, "").replace(/```/, "").trim()
+        );
+
+        if (!autoComplete.includes(phrases.join(" ")))
+          autoComplete.unshift(phrases.join(" "));
+
+        try {
+          const start = process.hrtime();
+          let toEval = eval(phrases.join(" "));
+          let hr = process.hrtime(start);
+
+          if (isPromise(toEval)) {
             toEval = await toEval;
-            hr = process.hrtime(hrstart);
+            hr = process.hrtime(start);
           }
 
-          let evaluated = inspect(toEval, false);
+          let depth = flags.get("depth") || 0;
+          if (isNaN(depth)) depth = 0;
 
-          evaluated = evaluated.toString();
+          let returned = inspect(toEval, false, depth).toString();
+
           if (powercord.account && powercord.account.token)
-            evaluated = evaluated.replace(powercord.account.token, replace);
+            returned = returned.replace(powercord.account.token, replace);
 
           return {
             send: false,
@@ -47,23 +79,43 @@ module.exports = class EvalCommand extends Plugin {
                 new RegExp("{time}", "gi"),
                 `${hr[0] > 0 ? `${hr[0]}s ` : ""}${hr[1] / 1000000}ms`
               )
-              .replace(new RegExp("{type}", "gi"), EvalCommand.type(toEval))
+              .replace(new RegExp("{type}", "gi"), type(toEval))
               .replace(
                 new RegExp(`{output}`, "gi"),
-                `\`\`\`js\n${evaluated.toString().substring(0, 1950)}\`\`\``
+                `\`\`\`js\n${returned.toString().substring(0, 1950)}\`\`\``
               )
               .replace(new RegExp("{n}", "gi"), "\n")
               .replace(
                 new RegExp("{input}", "gi"),
-                `\`\`\`js\n${args.join(" ")}\`\`\``
-              ),
+                `\`\`\`js\n${args
+                  .join(" ")
+                  .replace(/```js\n?.+\n?```/gi, "")
+                  .trim()}\`\`\``
+              )
+              .replace(new RegExp(`{depth}`, "gi"), depth)
+              .substring(0, 1999),
           };
         } catch (error) {
           return {
             send: false,
-            result: `Error:\`\`\`js${error}\`\`\``,
+            result: `\`\`\`js\n${error}\`\`\``,
           };
         }
+      },
+      autocomplete(args) {
+        if (!args || !args.length) return false;
+
+        const messages = powercord.pluginManager.plugins.get("pc-evalcommand")
+          .messages;
+
+        return {
+          header: `Auto Complete`,
+          commands: messages
+            .filter((msg) =>
+              msg.toLowerCase().includes(args.join(" ").toLowerCase())
+            )
+            .map((key) => ({ command: key, description: "" })),
+        };
       },
     });
   }
@@ -71,31 +123,6 @@ module.exports = class EvalCommand extends Plugin {
   pluginWillUnload() {
     powercord.api.commands.unregisterCommand("eval");
     powercord.api.settings.unregisterSettings("pc-evalcommand");
-  }
-
-  static type(value) {
-    const type = typeof value;
-    switch (type) {
-      case "object":
-        return value === null
-          ? "null"
-          : value.constructor
-          ? value.constructor.name
-          : "any";
-      case "function":
-        return `${value.constructor.name}(${value.length})`;
-      case "undefined":
-        return "void";
-      default:
-        return type;
-    }
-  }
-
-  static isPromise(value) {
-    return (
-      value &&
-      typeof value.then === "function" &&
-      typeof value.catch === "function"
-    );
+    delete powercord.pluginManager.plugins.get("pc-evalcommand").messages;
   }
 };
